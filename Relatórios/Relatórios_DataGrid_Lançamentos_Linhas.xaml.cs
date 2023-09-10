cdaa13b0eeca67522ddbaaa61d304f8fde6f34b9
Regis Andrade <regis.batista.andrade@outlook.com>
2023-09-09 23:07:15 -0300

    Inclusão de DataGrid de lançamentos + Designer da Tela de início

---
+++ Relatórios/Relatórios_DataGrid_Lançamentos_Linhas.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ECONOMIZE.Relatórios
+{
+    /// <summary>
+    /// Interação lógica para Relatórios_DataGrid_Lançamentos_Linhas.xam
+    /// </summary>
+    public partial class Relatórios_DataGrid_Lançamentos_Linhas : UserControl
+    {
+        public Relatórios_DataGrid_Lançamentos_Linhas()
+        {
+            InitializeComponent();
+        }
+    }
+}
